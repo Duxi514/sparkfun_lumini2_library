c6bb7696daae77bdcdb8166438a25d6b32804c42
Duxi514 <58293113+Duxi514@users.noreply.github.com>
2020-11-25 10:23:26 +0100

    Update colors.h

--- colors.h
+++ colors.h
@@ -16,80 +16,80 @@ void endframe(){
 	}
 }
 
-void red(int b){
-	brightness = set_brightness(b);
+void red(int b_red){
+	brightness = set_brightness(b_red);
 	send(brightness);
 	send(0);
 	send(0);
 	send(255);
 }
 
-void blue(int b){
-	brightness = set_brightness(b);
+void blue(int b_blue){
+	brightness = set_brightness(b_blue);
 	send(brightness);
 	send(255);
 	send(0);
 	send(0);
 }
 
-void green(int b){
-	brightness = set_brightness(b);
+void green(int b_green){
+	brightness = set_brightness(b_green);
 	send(brightness);
 	send(0);
 	send(255);
 	send(0);
 }
 
-void yellow(int b){
-	brightness = set_brightness(b);
+void yellow(int b_yellow){
+	brightness = set_brightness(b_yellow);
 	send(brightness);
 	send(0);
 	send(255);
 	send(255);
 }
 
-void purple(int b){
-	brightness = set_brightness(b);
+void purple(int b_purple){
+	brightness = set_brightness(b_purple);
 	send(brightness);
 	send(255);
 	send(0);
 	send(255);
 }
 
-void orange(int b){
-	brightness = set_brightness(b);
+void orange(int b_orange){
+	brightness = set_brightness(b_orange);
 	send(brightness);
 	send(0);
 	send(128);
 	send(255);
 }
 
-void cyan(int b){
-	brightness = set_brightness(b);
+void cyan(int b_cyan){
+	brightness = set_brightness(b_cyan);
 	send(brightness);
 	send(255);
 	send(255);
 	send(0);
 }
 
-void pink(int b){
-	brightness = set_brightness(b);
+void pink(int b_pink){
+	brightness = set_brightness(b_pink);
 	send(brightness);
 	send(255);
 	send(102);
 	send(255);
 }
 
-void grey(int b){
-	brightness = set_brightness(b);
+void grey(int b_grey){
+	brightness = set_brightness(b_grey);
 	send(brightness);
 	send(128);
 	send(128);
 	send(128);
 }
 
-void white(int b){
-	brightness = set_brightness(b);
+void white(int b_white){
+	brightness = set_brightness(b_white);
 	send(brightness);
 	send(255);
 	send(255);
